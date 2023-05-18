7cc984cde3ee5c2229862c494b698457d3f07481
Nick <nick.chen@cmp.com.tw>
2023-05-18 09:49:27 +0800

    Initial commit

---
+++ src/main/java/tw/niq/example/ExampleSpringMicroDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringMicroDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringMicroDiscoveryServiceApplication.class, args);
+	}
+
+}
