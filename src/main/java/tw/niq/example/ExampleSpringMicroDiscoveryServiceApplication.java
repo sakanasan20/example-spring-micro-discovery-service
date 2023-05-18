cc154715cea54757704ae9ba092b78eb6093d298
Nick <nick.chen@cmp.com.tw>
2023-05-18 09:57:59 +0800

    Enable and configure eureka server

--- src/main/java/tw/niq/example/ExampleSpringMicroDiscoveryServiceApplication.java
+++ src/main/java/tw/niq/example/ExampleSpringMicroDiscoveryServiceApplication.java
@@ -2,7 +2,12 @@ package tw.niq.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/*
+ * Step 1. Enable Eureka Server
+ */
+@EnableEurekaServer
 @SpringBootApplication
 public class ExampleSpringMicroDiscoveryServiceApplication {
 
